658340fc7e3ca78ef7c664f3a2f7d000a4165270
Adel LARBI <AdelLarbi@users.noreply.github.com>
2017-11-13 12:27:44 +0100

    Update bd_mondiale : New table "Videos"

--- db_mondiale.sql
+++ db_mondiale.sql
@@ -64,6 +64,34 @@ CREATE TABLE IF NOT EXISTS `bdd_mondiale`.`Users_Robots` (
 ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `bdd_mondiale`.`Videos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bdd_mondiale`.`Videos` (
+  `idVideo` INT NOT NULL AUTO_INCREMENT,
+  `urlImage` VARCHAR(45) NOT NULL,
+  `urlVideo` VARCHAR(45) NOT NULL,
+  `duration` VARCHAR(45) NOT NULL,
+  `date` VARCHAR(45) NOT NULL,
+  `title` VARCHAR(45) NOT NULL,
+  `idUser` INT NOT NULL,
+  `idRobot` INT NOT NULL,
+  PRIMARY KEY (`idVideo`, `urlImage`, `urlVideo`, `idUser`, `idRobot`),
+  INDEX `fk_Videos_Users1_idx` (`idUser` ASC),
+  INDEX `fk_Videos_Robots1_idx` (`idRobot` ASC),
+  CONSTRAINT `fk_Videos_Users1`
+    FOREIGN KEY (`idUser`)
+    REFERENCES `bdd_mondiale`.`Users` (`idUser`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Videos_Robots1`
+    FOREIGN KEY (`idRobot`)
+    REFERENCES `bdd_mondiale`.`Robots` (`idRobot`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
